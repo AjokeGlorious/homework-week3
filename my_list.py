c82c86d5ae89a1a99b5b2b04b4a6d14657c041e3
Ajoke <131398430+AjokeGlorious@users.noreply.github.com>
2024-03-27 14:12:08 +0100

    Add files via upload

---
+++ my_list.py
@@ -0,0 +1,8 @@
+
+integer_list = ([10,20,30,40])
+total_sum = sum(integer_list)
+print(f"the sum of the integers in the list is: {total_sum}")
+integer_list [2] = 15
+print(integer_list)
+new_list = ([10,20,30,40,50,60,70])
+print(new_list)
